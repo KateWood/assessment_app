1199bbe53ecabe5214a0f8ccd36c1506a9f4a15a
Kate Wood <katewood611@gmail.com>
2017-03-15 20:45:31 -0700

    adds basic playback functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 console.log('connected')
+
+$(document).ready(function() {
+  var events = []
+  $('#play').click(function() {
+    $('textarea').val('')
+    console.log('Playback started')
+    var playBack = setInterval(function() {
+      var currEvent = events.shift()
+      console.log(currEvent)
+      $('textarea').val(currEvent.textarea)
+      if(events.length < 1) {
+        clearInterval(playBack)
+        console.log('Playback complete')
+      }
+    }, 1000)
+  })
+
+  $('textarea').keyup(function(e) {
+    if (e.keyCode == 16) {
+      console.log('Shift')
+    } else {
+      var newEvent = {
+        timeStamp: e.timeStamp,
+        textarea: $('textarea').val()
+      }
+      console.log(newEvent.textarea)
+      events.push(newEvent)
+    }
+  })
+})
